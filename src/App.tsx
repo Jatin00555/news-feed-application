196790dd13a983cf883328c6f51cf4d431bd5048
jatin.kumar <jatinkumar@uniqus.com>
2025-02-19 22:19:04 +0530

    -Added global state management flow -Worked on store setup -Added provided and provided store to APP.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import NewsAggregator from "./components/NewsAggregator";
+import { store } from "./storage/globalStore/store";
 
 function App() {
-  return <NewsAggregator />;
+  return (
+    <Provider store={store}>
+      <NewsAggregator />
+    </Provider>
+  );
 }
 
 export default App;
