ae9bdf0617ee4be3f013629e8daa6047beeb90b1
jatin.kumar <jatinkumar@uniqus.com>
2025-02-21 20:08:04 +0530

    -Worked on storing user preference

--- src/storage/slices/filterSlice.ts
+++ src/storage/slices/filterSlice.ts
@@ -1,17 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { FilterState } from "../../types/sliceTypes";
 
-const initialState: FilterState = {
-  query: "",
-  source: [],
-  category: [],
-  author: [],
-  timeLine: null,
+const loadFilters = (): FilterState => {
+  const storedFilters = localStorage.getItem("filters");
+  return storedFilters
+    ? JSON.parse(storedFilters)
+    : { query: "", source: [], category: [], author: [], timeLine: null };
 };
 
 const filterControl = createSlice({
   name: "filters",
-  initialState,
+  initialState: loadFilters(),
   reducers: {
     setSearchQuery: (state, action) => {
       state.query = action.payload;
@@ -30,6 +29,10 @@ const filterControl = createSlice({
     },
   },
 });
+
+export const persistFiltersToLocalStorage = (state: FilterState) => {
+  localStorage.setItem("filters", JSON.stringify(state));
+};
 
 export const {
   setAuthor,
